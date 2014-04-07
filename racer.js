bea79c0044e79ed2217596552d1c3ca0d98bd1c8
Jennifer Rygh & Rori_Manubens <pair+jrygh00.manubete@devbootcamp.com>
2014-04-07 12:20:14 -0700

    Wrote basic move function   * moves one player forward one space   * changes the previous cell color to show it has been crossed   * gives the active cell the active class and makes it black

---
+++ racer.js
@@ -0,0 +1,25 @@
+
+// we have a racetracke with td
+// start with location = zero
+// in td coooresponding to location
+// make class active
+// all other locations class = open or null
+
+
+// for each keystroke add to location 1
+// switch players
+// when location is equal to length of track game over
+// declare winner
+
+var array_of_spaces = document.querySelector("#player1_strip");
+var child_space = array_of_spaces.childNodes[0]
+// var active_child_space
+
+function move() {
+
+  // locations and spaces are different ways to access the player1 strip and active cell
+  var active_space_p1 = document.querySelector("#player1_strip").querySelector(".active");
+  var current_cell_p1 = active_space_p1.nextElementSibling.className = "active";
+  active_space_p1.className="crossed";
+}
+
