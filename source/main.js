09cadbdb78655616cef6daf7e05685aca831e352
& <pair+Keopaseutb.drewfwest@devbootcamp.com>
2014-04-07 16:29:19 -0700

    update_player_position function    *Takes two parameters - player and position    *Attaches css-class "active" to the specified cell (given by position) in the player row
    TODO
       * Set Binding to Key Presses
          * Implement method to listen for key strokes
          * Find the active td in player row
          * Find a method to move to the next element in the doc

--- source/main.js
+++ source/main.js
@@ -1,10 +1,34 @@
-document.onreadystatechange = function() {
-  if (document.readyState == 'complete')
-  for (index=0; index<20;index++){
-    console.log('done this many times', index)
-    var blank_table_data = document.createElement("TD")
-    document.getElementById("player1_row").appendChild(blank_table_data);
-    var blank_table_data = document.createElement("TD")
-    document.getElementById("player2_row").appendChild(blank_table_data);
+var setupLanes = function(number_of_cells_in_lane){
+  for (index=0; index<number_of_cells_in_lane; index++){
+   document.getElementById("player1_row").appendChild(document.createElement('TD'));
+   document.getElementById("player2_row").appendChild(document.createElement('TD'));
   }
 }
+ var update_player_position = function(player, position){
+  document.querySelector("#" + player).querySelectorAll("td")[position].className = 'active';
+ }
+
+current_position = 0
+
+document.onreadystatechange = function() {
+  if (document.readyState == 'complete'){
+    setupLanes(20);
+    update_player_position('player1_row', current_position);
+    update_player_position('player2_row', current_position);
+  }
+}
+
+
+
+
+// TODO
+// event listener to hear a key stroke
+// Move the player on each key stroke
+//   * Find the td which is active
+//   * Look up the method to move to the next element in the document
+
+
+
+
+
+
