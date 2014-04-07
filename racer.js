f9cb35967e4798497456d43ae5226d4e74a35561
Ashwin Kumar & Lyndsey Williams <pair+ashnkumar.lyndseywilliams@devbootcamp.com>
2014-04-07 15:59:19 -0700

    announcing winner

--- racer.js
+++ racer.js
@@ -2,31 +2,43 @@ var strip1 = document.querySelector("#player1_strip")
 
 var strip2 = document.querySelector("#player2_strip")
 
+
+
 var movePlayer = function(strip){
   var currentActiveSpot = findPlayer(strip)
   var newActiveSpot = currentActiveSpot.nextElementSibling
   newActiveSpot.className = "active"
   currentActiveSpot.className = ""
-
-}
+};
 
 var findPlayer = function(strip){
   var currentActiveSpot = strip.querySelector('.active')
   return currentActiveSpot
 };
 
-// var declareWinner = function(strip) {
-//   if (
-//       alert("winner!!")
+// var checkWinner = function(strip, currentActiveSpot) {
+//   var lastBox = strip.lastElementChild
+//     if (currentActiveSpot == lastBox)
+//        alert("winner!!")
 // }
+
+
+var checkWinner = function(strip) {
+  var currentActiveSpot = findPlayer(strip)
+  var lastBox = strip.lastElementChild
+  if (currentActiveSpot.className === lastBox.className){
+     alert( "!!!")
+  }
+}
 
 document.onkeyup = function(e) {
   if (e.keyCode == 80){
     movePlayer(strip1)
+    checkWinner(strip1)
   }
-
-  if (e.keyCode == 81) {
+  else if (e.keyCode == 81) {
     movePlayer(strip2)
-  }
+    checkWinner(strip2)
+  };
 };
 
