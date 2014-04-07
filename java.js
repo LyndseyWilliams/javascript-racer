3ecb0869629c95018f75e6652ef2f8ff87577330
Greg Gerla & Paolo Payumo <pair+gregorygerla.paopay@devbootcamp.com>
2014-04-07 15:58:06 -0700

    we can race 2 players

--- java.js
+++ java.js
@@ -1,37 +1,69 @@
+// document.querySelector('#MyElement').addEventListener('click', changeA);
 document.onreadystatechange = function() {
+//set up
+  document.addEventListener('keyup', pressM, false)
+  document.addEventListener('keyup', pressQ, false)
 
-  document.querySelector('#MyElement').addEventListener('click', changeA);
+
 }
-
+//functions for playerA to turn off current
   function turnCurrentOff(){
   if (document.querySelector('#player1_strip .activeAgo')) {
-    a = document.querySelector('#player1_strip .activeAgo').className = 'activeAtouched';
-    console.log(a)
+    document.querySelector('#player1_strip .activeAgo').className = 'activeAtouched';
+
     }
   }
-
+//functions for playerA to turn on current
   function turnFirstOn() {
   if (document.querySelector('#player1_strip .activeA')) {
-    b = document.querySelector('#player1_strip .activeA').className = 'activeAgo';
-    console.log(b)
+    document.querySelector('#player1_strip .activeA').className = 'activeAgo';
+
+    }
+  }
+// functions for playerB to turn off current
+  function turnMCurrentOff(){
+  if (document.querySelector('#player2_strip .activeMgo')) {
+    document.querySelector('#player2_strip .activeMgo').className = 'activeMtouched';
+
+    }
+  }
+// functions for playerB to turn on current
+  function turnMFirstOn() {
+  if (document.querySelector('#player2_strip .activeM')) {
+   document.querySelector('#player2_strip .activeM').className = 'activeMgo';
+
     }
   }
 
-  function changeA(){
-    console.log("hey")
+//player A master manipulating function
+  function changeQ(){
     turnCurrentOff()
     turnFirstOn()
   }
+//player B master manipulating function
+  function changeM(){
+    turnMCurrentOff()
+    turnMFirstOn()
+  }
 
 
-// function changeB(){
-//   if (document.querySelector('.activeBgo')) {
-//     document.querySelector('.activeBgo').className = 'activeB';
-//   }
-//   if (document.querySelector('.activeB')) {
-//    document.querySelector('.activeB').className += 'go';
-//   }
-// }
+
+//key pressing functions
+  function pressQ(callback){
+    if (callback.keyCode == 81) {
+      changeQ();
+    }
+  }
+  function pressM(callback){
+    if (callback.keyCode == 77) {
+      changeM();
+    }
+  }
+
+
+
+
+
 
 
 
