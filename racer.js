7eee2eaa93bf1a1fc5f5d4eb1c8ff51468cf6b37
Jennifer Rygh & Rori_Manubens <pair+jrygh00.manubete@devbootcamp.com>
2014-04-07 16:52:04 -0700

    Added a key binding for the movement     * When a key is pressed up, it triggers one of the two move player functions

--- racer.js
+++ racer.js
@@ -11,15 +11,34 @@
 // when location is equal to length of track game over
 // declare winner
 
-var array_of_spaces = document.querySelector("#player1_strip");
-var child_space = array_of_spaces.childNodes[0]
+// var array_of_spaces = document.querySelector("#player1_strip");
+// var child_space = array_of_spaces.childNodes[0]
 // var active_child_space
 
-function move(player_number) {
 
-  // locations and spaces are different ways to access the player1 strip and active cell
-  var active_space_p1 = document.querySelector("#player" + player_number + "_strip").querySelector(".active");
+//Move player 1 /corresponds to p key (code 80)
+function move1() {
+  var active_space_p1 = document.querySelector("#player1_strip").querySelector(".active");
+  var current_cell_p1 = active_space_p1.nextElementSibling.className = "active";
+  active_space_p1.className="crossed";
+}
+//Move player Two corresponds to q key (code 81)
+function move2() {
+  var active_space_p1 = document.querySelector("#player2_strip").querySelector(".active");
   var current_cell_p1 = active_space_p1.nextElementSibling.className = "active";
   active_space_p1.className="crossed";
 }
 
+
+document.onkeyup = function(e){
+  console.log(e.keyCode);
+if (e.keyCode==80){move1()};
+if (e.keyCode==81){move2()}
+};
+
+// //This works, too
+//  document.onkeyup = function(e) {
+//   if(e.keyCode==80){
+//   document.addEventListener('keyup', move1()); //could put false at the end of it, if you wanna execute the function, make sure its a function not a result
+//    }
+// };
