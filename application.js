382a4aa3540907b1af53754362bced37281a1dc5
laura & Tyler Stauss <pair+llcossette.tylerstauss@devbootcamp.com>
2014-04-07 16:21:12 -0700

    dynamic game board length

--- application.js
+++ application.js
@@ -1,8 +1,43 @@
+var lengthOfTrack = 0;
+
+function setLength(){
+lengthOfTrack = document.getElementById("length").value;
+setGameBoard();
+}
+
+function setGameBoard(){
+
+var player1 = document.getElementById('player1');
+var player2 = document.getElementById('player2');
+
+
+console.log(player1);
+console.log(player2);
+console.log(lengthOfTrack);
+
+
+
+for (var i = 1; i <= lengthOfTrack; i++) {
+  var td = document.createElement('td');
+  var p1td = player1.appendChild(td);
+  p1td.id = '1_' + i;
+
+  var td2 = document.createElement('td');
+  var p2td = player2.appendChild(td2);
+  p2td.id = '2_' + i;
+};
+};
+
 var player1_position = [1,1];
 var player2_position = [1,1];
 
+
+
+
+
+
 var gameOver = false;
-console.log(gameOver);
+
 document.onreadystatechange = function() {
     document.addEventListener('keyup', playGame, false)
 
@@ -21,21 +56,21 @@ function playGame(event) {
    else {
     return '';
    }
-  if (player1_position[player1_position.length - 1] == 30 || player2_position[player2_position.length - 1] == 30) {
+  if (player1_position[player1_position.length - 1] == lengthOfTrack || player2_position[player2_position.length - 1] == lengthOfTrack) {
     declareWinner();
   }
 }
 
 
 function declareWinner() {
-  if (player1_position[player1_position.length - 1] == 30 && player2_position[player2_position.length - 1] == 30) {
+  if (player1_position[player1_position.length - 1] == lengthOfTrack && player2_position[player2_position.length - 1] == lengthOfTrack) {
     document.getElementById('winner').innerHTML="It's a Tie!";
 
   }
-  else if (player1_position[player1_position.length - 1] == 30) {
+  else if (player1_position[player1_position.length - 1] == lengthOfTrack) {
     document.getElementById('winner').innerHTML="Player 1 Wins!";
   }
-  else if (player2_position[player2_position.length - 1] == 30) {
+  else if (player2_position[player2_position.length - 1] == lengthOfTrack) {
     document.getElementById('winner').innerHTML="Player 2 Wins!";
   }
   document.removeEventListener('keyup', playGame, false);
@@ -59,8 +94,8 @@ function getNewPosition(player) {
     console.log('player 1');
     console.log(player1_position);
     // debugger
-    if (player1_position[player1_position.length - 1] > 30) {
-      player1_position[player1_position.length - 1] = 30;
+    if (player1_position[player1_position.length - 1] > lengthOfTrack) {
+      player1_position[player1_position.length - 1] = lengthOfTrack;
     }
     updatePlayerPosition(player, player1_position[player1_position.length - 1], player1_position[player1_position.length - 2]);
   }
@@ -68,8 +103,8 @@ function getNewPosition(player) {
     player2_position.push(roll() + player2_position[player2_position.length - 1]);
     console.log('player 2');
     console.log(player2_position);
-    if (player2_position[player2_position.length - 1] > 30) {
-      player2_position[player2_position.length - 1] = 30;
+    if (player2_position[player2_position.length - 1] > lengthOfTrack) {
+      player2_position[player2_position.length - 1] = lengthOfTrack;
     }
     updatePlayerPosition(player, player2_position[player2_position.length - 1], player2_position[player2_position.length - 2]);
   }
