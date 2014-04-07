95485ed66a58bdb69df2e6c69d44b214ec48d2e1
Charles Arciniega & Nina Hwang <pair+arcin.nihwang@devbootcamp.com>
2014-04-07 12:14:04 -0700

    charles is silly. unrefactored-ly silly

--- source/script.js
+++ source/script.js
@@ -8,7 +8,19 @@ var update_player_position = function(trackName, position)
 {
   var entireTrack = document.querySelectorAll(trackName);
   clearTrack(entireTrack);
-  entireTrack[position - 1].classList.add('active')
+  entireTrack[position].classList.add('active')
 }
 
-update_player_position('div.blue', 11);
+
+pPressed = 0;
+qPressed = 0;
+document.addEventListener('keyup', function(event){
+  console.log("something");
+  if (event.keyCode == 80){
+    pPressed++;
+  } else if (event.keyCode == 81) {
+    qPressed++;
+  }
+  update_player_position('div.blue', pPressed);
+  update_player_position('div.green', qPressed);
+}, false)
