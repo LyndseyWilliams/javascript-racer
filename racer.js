872f5ad761b2b4bcf453546f187bdf1e96573581
Jennifer Rygh & Rori_Manubens <pair+jrygh00.manubete@devbootcamp.com>
2014-04-07 12:25:06 -0700

    Made Move function work for several players  * Move function interpolates the player number with the  * corresponding strip.

--- racer.js
+++ racer.js
@@ -15,10 +15,10 @@ var array_of_spaces = document.querySelector("#player1_strip");
 var child_space = array_of_spaces.childNodes[0]
 // var active_child_space
 
-function move() {
+function move(player_number) {
 
   // locations and spaces are different ways to access the player1 strip and active cell
-  var active_space_p1 = document.querySelector("#player1_strip").querySelector(".active");
+  var active_space_p1 = document.querySelector("#player" + player_number + "_strip").querySelector(".active");
   var current_cell_p1 = active_space_p1.nextElementSibling.className = "active";
   active_space_p1.className="crossed";
 }
