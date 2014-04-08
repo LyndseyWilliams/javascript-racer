84e17e8067195502c7615032b168bdcacca08590
& <pair+Keopaseutb.drewfwest@devbootcamp.com>
2014-04-07 23:37:15 -0700

    Adding trackLength form to allow user to determine track length.

--- source/main.js
+++ source/main.js
@@ -1,6 +1,6 @@
-player1Position = 0
-player2Position = 0
-boardLength = 20
+var player1Position = 0
+var player2Position = 0
+var boardLength = 20
 
 var setupLanes = function(number_of_cells_in_lane){
   for (index=0; index<number_of_cells_in_lane; index++){
@@ -13,32 +13,23 @@ var setupLanes = function(number_of_cells_in_lane){
 
 var movePlayerPosition = function(player, position){
   var playerRow = document.querySelector("#" + player).querySelectorAll("td")
-  // determineWinner(playerRow)
   if (position > 0 && position < playerRow.length ){
     playerRow[position - 1 ].className = '';
   } else if (position > playerRow.length) {
     alert(player + " won");
   }
-
   if (position < playerRow.length)
   {
     playerRow[position].className = 'active';
   }
-  // determineWinner(params)
-// document.querySelector("#" + player).querySelectorAll("td")[position].className = 'active';
 }
-
-// var determineWinner = function(playerRow){
-//   if playerRow > setupLanes - 1
-//     alert('')
-// }
 
 var listenKeystroke = function(event) {
-if (event.keyCode == 80)
-  movePlayerPosition('player1', player1Position = player1Position + 1)
-else if (event.keyCode == 81)
-  movePlayerPosition('player2', player2Position = player2Position + 1)
-}
+  if (event.keyCode == 80)
+    movePlayerPosition('player1', player1Position = player1Position + 1)
+  else if (event.keyCode == 81)
+    movePlayerPosition('player2', player2Position = player2Position + 1)
+  }
 
 document.onreadystatechange = function() {
     document.addEventListener("keyup", listenKeystroke, false);
@@ -50,5 +41,3 @@ document.onreadystatechange = function() {
   }
 
 }
-
-// Start(boardLength)
