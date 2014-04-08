7a1417cbfab5dd2de1a7439b314c06bc9a4319b9
Brittan McGinnis <konamax123@gmail.com>
2014-04-07 18:33:29 -0700

    able to display winner

--- racer.js
+++ racer.js
@@ -1,27 +1,37 @@
 $(document).ready(function(){
 
+document.addEventListener('keyup', racer, false)
 
-var movePlayer = function(player){
-    player.classList.remove('active');
-    player.nextElementSibling.classList.add('active');
-  }
 
-document.onkeyup = function(event){
+})
+
+
+
+var racer = function(event){
   var keyPress = String.fromCharCode(event.keyCode);
   if (keyPress == "Q") {
-  var player1 = document.querySelector('#player1_strip > td.active')
+  var player1 = document.querySelector('#player1 > td.active')
   movePlayer(player1)
 
 
   }else if (keyPress == "P"){
-  var player2 = document.querySelector('#player2_strip > td.active')
+  var player2 = document.querySelector('#player2 > td.active')
   movePlayer(player2)
+  }
+  
+}
+var movePlayer = function(player){
+    player.classList.remove('active');
+    var nextCell = player.nextElementSibling;
+    nextCell.classList.add('active')
+    checkWinner(nextCell)
   }
 
 
-
-}
-
-
-
-})
+  var checkWinner = function(nextCell){
+    if (nextCell.classList.contains("finish")) {
+      var display = (nextCell.parentElement.id + " WINNNER")
+      document.getElementById('showWinner').innerHTML = display
+      document.removeEventListener('keyup', racer)
+    };
+  }
