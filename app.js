b7e703916144941744568d56327082fa067e72fc
Apprentice <apprentice@dbc25.local>
2014-04-07 17:47:05 -0700

    handles multiple users, variable track length

--- app.js
+++ app.js
@@ -1,17 +1,64 @@
-var setupLanes = function(number_of_cells_in_lane){
-  for (index=0; index<number_of_cells_in_lane;index++){
-    document.getElementById("player1").appendChild(document.createElement("TD"));
-    document.getElementById("player2").appendChild(document.createElement("TD"));
-  }
-}
-
-var update_player_position = function(player, position){
-  document.querySelector("#" + player).querySelectorAll("td")[position].class = 'active';
-}
-
 document.onreadystatechange = function(){
   if (document.readyState == "complete"){
-    setupLanes(20);
-    update_player_position('player1', 10)
+    setupLanes(10);
+    document.addEventListener('keyup', handleKeyup, false)
   }
+}
+
+
+
+//---------------HELPER METHODS---------------------------------------------
+
+//---------------SETUP
+var setupLanes = function(number_of_cells_in_lane){
+  for (index=0; index<number_of_cells_in_lane;index++){
+    document.getElementById("player1").appendChild(makeNewTD(index));
+    document.getElementById("player2").appendChild(makeNewTD(index));
+  }
+}
+
+//---------------EVENT HANDLING CODE---------------
+var handleKeyup = function(keyUpEvent){
+  var keyPressed = String.fromCharCode(keyUpEvent.keyCode)
+  console.log(keyPressed)
+  if (keyPressed == "Q"){
+    update_player_position('player1')
+  }else if(keyPressed == "P"){
+    update_player_position('player2')
+  }
+}
+
+var update_player_position = function(player){
+  new_position = current_position(player) + 1
+  all_tds_for_given_player(player)[current_position(player)].className = '';
+  all_tds_for_given_player(player)[new_position].className = 'active';
+}
+
+//---------------SETUP HELPER---------------
+
+var makeNewTD = function(index){
+  var new_table_data = document.createElement("td")
+    if (index == 0){
+      new_table_data.className = "active"
+    }
+  return new_table_data
+}
+
+//---------------UPDATE HELPER---------------
+
+var lane_of = function(player){
+  return document.querySelector("#" + player)
+}
+
+var active_square = function(player){
+  return lane_of(player).querySelector("td.active");
+}
+
+var all_tds_for_given_player = function(player){
+  return lane_of(player).querySelectorAll("td");
+}
+
+var current_position = function(player){
+  var tds_as_array = Array.prototype.slice.call(all_tds_for_given_player(player))
+  return tds_as_array.indexOf(active_square(player));
 }
---
+++ dynamicplayers.js
@@ -0,0 +1,70 @@
+document.onreadystatechange = function(){
+  if (document.readyState == "complete"){
+    setupLanes(10, 4);
+    document.addEventListener('keyup', handleKeyup, false)
+  }
+}
+
+
+
+//---------------HELPER METHODS---------------------------------------------
+
+//---------------SETUP---------------
+var setupLanes = function(numberOfCellsInLane, numberOfPlayers){
+
+  for (playerIndex=1; playerIndex<=numberOfPlayers;playerIndex++) {
+    var playerRow = document.createElement("tr");
+    playerRow.id = "player" + playerIndex;
+    for (cellIndex=0; cellIndex<numberOfCellsInLane;cellIndex++){
+      playerRow.appendChild(makeNewTD(cellIndex))
+    }
+    document.querySelector(".racer_table").appendChild(playerRow);
+  }
+
+}
+
+//---------------EVENT HANDLING CODE---------------
+var handleKeyup = function(keyUpEvent){
+  var keyPressed = String.fromCharCode(keyUpEvent.keyCode)
+  console.log(keyPressed)
+  if (keyPressed == "Q"){
+    updatePlayerPosition('player1')
+  }else if(keyPressed == "P"){
+    updatePlayerPosition('player2')
+  }
+}
+
+var updatePlayerPosition = function(player){
+  newPosition = currentPosition(player) + 1
+  allTdsForGivenPlayer(player)[currentPosition(player)].className = '';
+  allTdsForGivenPlayer(player)[newPosition].className = 'active';
+}
+
+//---------------SETUP HELPER---------------
+
+var makeNewTD = function(index){
+  var newTableData = document.createElement("td")
+    if (index == 0){
+      newTableData.className = "active"
+    }
+  return newTableData
+}
+
+//---------------UPDATE HELPER---------------
+
+var lane_of = function(player){
+  return document.querySelector("#" + player)
+}
+
+var activeSquare = function(player){
+  return lane_of(player).querySelector("td.active");
+}
+
+var allTdsForGivenPlayer = function(player){
+  return lane_of(player).querySelectorAll("td");
+}
+
+var currentPosition = function(player){
+  var tdsAsArray = Array.prototype.slice.call(allTdsForGivenPlayer(player))
+  return tdsAsArray.indexOf(activeSquare(player));
+}
